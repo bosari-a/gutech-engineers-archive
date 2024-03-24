e2eb25dc0bafd4b8fbdb0e2b3f52700affceaf2f
bosari-a <ahmedelbosarimc@gmail.com>
2024-03-24 14:19:41 +0400

    using express with ejs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path/posix");
 
-let ps = fs.readdirSync("./").filter((p) => !p.includes("."));
+let ps = fs.readdirSync("./static/").filter((p) => !p.includes("."));
 ps.map((p) => {
   let new_p = p;
   if (p.includes(" ")) {
@@ -23,7 +23,7 @@ class Doc {
 for (let j = 0; j < ps.length; j++) {
   const p = ps[j];
   const arr = [];
-  const d = fs.readdirSync(p);
+  const d = fs.readdirSync(path.join("./static", p));
   for (let i = 0; i < d.length; i++) {
     const f = d[i];
     if (f.includes("png") || !f.includes(".")) {
@@ -39,18 +39,13 @@ for (let j = 0; j < ps.length; j++) {
     imgf.pop();
     imgf = imgf.join(".") + ".png";
     const imgpath = path.join(p, imgf);
-    const obj = new Doc(
-      f,
-      f,
-      `/web/viewer.html?file=https://github.com/bosari-a/gutech-engineers-archive/raw/main/${new_fp}`,
-      imgpath
-    );
+    const obj = new Doc(f, f, new_fp, imgpath);
     arr.push(obj);
   }
   const subject = {
     name: p,
     docs: arr,
   };
-  const json_out = path.join(p, `${path.basename(p)}.json`);
+  const json_out = path.join("./static", p, `${path.basename(p)}.json`);
   fs.writeFileSync(json_out, JSON.stringify(subject), { encoding: "utf-8" });
 }
